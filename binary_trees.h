b611e8883ab929ee2c46f65a0498f363a18aacab
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 01:46:11 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -18,9 +18,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
