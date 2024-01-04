c21e6872f39039d7b0947cba1445c5b4d075ca49
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 19:34:07 +0100

    Create 8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+* binary_tree_postorder - tranverse and prints all num in a tree
+* using in-order transversal
+* @tree: tree to tranverse
+* @func: func pointer to print function
+*
+* Return: Void
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
