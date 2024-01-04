3e2e547cc1f7e808f3261dbe9891f331a705d6f3
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 20:12:56 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,8 +7,10 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (!tree || !tree->left || !tree->right)
+        if (!tree)
 		return (0);
+	if (!tree->left || !tree->right)
+		return (2);
         return (1 + (binary_tree_height(tree->right) + 
 binary_tree_height(tree->left)) / 2);
 }
