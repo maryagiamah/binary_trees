c0091ee9426fdc50dfb57e2161b8fcc5b6157f3e
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 00:45:00 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,7 +16,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	/* Return the maximum height of left or right subtree, plus 1 for the current node */
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
