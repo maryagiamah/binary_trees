9533468c2fab1f7861e523c94baeb4a686ede27b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 01:34:08 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,24 +7,26 @@
 *
 * Return: pointer to node or NULL
 */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
-    if (!parent)
-    {
-        return (NULL);
-    }
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
 
-    binary_tree_t *new_node = binary_tree_node(parent, value);
-    if (!new_node)
-    {
-        return (NULL);
-    }
+	if (!parent)
+	{
+		return (NULL);
+	}
 
-    if (parent->left)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+	{
+		return (NULL);
+	}
 
-    parent->left = new_node;
-    return (new_node);
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
