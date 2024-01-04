93d0dd48e584408160a8f48cf0d282bccff0d36a
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 00:33:28 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 /**
-* binary_tree_nodes - returns height of a tree
+* binary_tree_height - returns height of a tree
 * @tree: tree to determine height
 *
 * Return: height of tree
 */
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (0);
-	return (1 + binary_tree_nodes(tree->right) +
-binary_tree_nodes(tree->left));
+	return (1 + (binary_tree_height(tree->right) +
+binary_tree_height(tree->left) + 1) / 2);
 }
 
 /**
@@ -25,6 +25,6 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_nodes(tree->left) -
-binary_tree_nodes(tree->right));
+	return (binary_tree_height(tree->left) -
+binary_tree_height(tree->right));
 }
