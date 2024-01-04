fcf73ef42383ac9823263e94aca62cd78aec0ba7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 01:09:31 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,16 +11,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
-	if (!value)
-		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
-	new->parent = NULL;
+	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
 	new->n = value;
-	if (parent)
-		new->parent = parent;
+
 	return (new);
 }
