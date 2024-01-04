65ef950403a67329c4f6b600b0eb2d73af2eca0a
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 22:35:03 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,7 +27,7 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		return (-1)
+		return (-1);
 	return (binary_tree_height(tree->left) -
 binary_tree_height(tree->right));
 }
