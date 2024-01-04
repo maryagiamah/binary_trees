26285f6dcf5b98838e460feeb6089b34093d6bc6
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-04 21:57:23 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_balance(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
