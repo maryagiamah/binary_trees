e575ed9685657156fb50e00c0c7d1ce429d89a9e
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 13:27:47 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,18 @@
 
+#include "binary_trees.h"
+/**
+* binary_tree_size - returns size of a tree
+* @tree: tree to determine height
+*
+* Return: height of tree
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->right) +
+binary_tree_size(tree->left));
+}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
