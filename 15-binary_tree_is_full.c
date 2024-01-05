7d2121138a4fb3159eca56dbf6d058f191006c62
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 02:18:26 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,26 +1,5 @@
 #include "binary_trees.h"
 /**
-* binary_tree_nodes_full - returns height of a tree
-* @tree: tree to determine height
-*
-* Return: height of tree
-*/
-size_t binary_tree_nodes_full(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	/* Calculate the heights of left and right subtrees */
-	int left_node = binary_tree_nodes_full(tree->left);
-	int right_node = binary_tree_nodes_full(tree->right);
-
-	/* Calculate and return the balance factor */
-	return (left_node && right_node);
-}
-
-
-/**
 * binary_tree_is_full - returns size of a tree
 * @tree: tree to determine height
 *
@@ -30,9 +9,12 @@ size_t binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	int left_node = binary_tree_nodes_full(tree->left);
-	int right_node = binary_tree_nodes_full(tree->right);
+	if (!tree->left && !tree->right)
+		return (1);
+	/* Calculate the heights of left and right subtrees */
+	int left_node = binary_tree_is_full(tree->left);
+	int right_node = binary_tree_is_full(tree->right);
 
-	/* tells if it's a full binary tree */
+	/* Calculate and return the balance factor */
 	return (left_node && right_node);
 }
