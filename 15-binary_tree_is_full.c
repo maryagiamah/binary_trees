b425dc5bef231637ee427f77d03d64c3d9ac5da2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 01:09:22 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,7 +5,7 @@
 *
 * Return: height of tree
 */
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_nodes_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
@@ -13,4 +13,21 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	return (1 + binary_tree_nodes(tree->right) +
 binary_tree_nodes(tree->left));
+}
+
+/**
+* binary_tree__is_full - returns size of a tree
+* @tree: tree to determine height
+*
+* Return: height of tree
+*/
+size_t binary_tree__is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	int left_node = binary_tree_nodes(tree->left);
+	int right_node = binary_tree_nodes(tree->right);
+
+	/* tells if it's a full binary tree */
+	return ((left_node + right_node) % 2);
 }
