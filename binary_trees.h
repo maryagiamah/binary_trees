0c81b08c0e394b97c8a9750cbe35727650719fe8
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 01:17:05 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -33,6 +33,7 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_is_full(const binary_tree_t *tree);
 void print_num(int n);
 size_t binary_tree_nodes_full(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
