cacba543de4a0f1f7f245086d20ffd0c32eb7fbb
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 02:30:52 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 /**
-* binary_tree_is_full - returns size of a tree
+* binary_tree_is_perfect - returns size of a tree
 * @tree: tree to determine height
 *
 * Return: height of tree
 */
-size_t binary_tree_is_full(const binary_tree_t *tree)
+size_t binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	/* Calculate the heights of left and right subtrees */
-	int left_node = binary_tree_is_full(tree->left);
-	int right_node = binary_tree_is_full(tree->right);
+	int left_node = binary_tree_is_perfect(tree->left);
+	int right_node = binary_tree_is_perfect(tree->right);
 
 	/* Calculate and return the balance factor */
 	return (left_node && right_node);
