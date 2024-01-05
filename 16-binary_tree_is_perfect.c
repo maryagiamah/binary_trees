1b2bfd97ddf028dd12b1432a56069cb0a575b5c2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 12:32:21 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,8 +9,10 @@ size_t binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (tree->left ^ tree->right)
+	if (!tree->left ^ !tree->right)
 		return (0);
+	if (!tree->left && !tree->right)
+               return (1);
 	return (1 && (binary_tree_is_perfect(tree->left)
 	 && binary_tree_is_perfect(tree->right)));
 }
