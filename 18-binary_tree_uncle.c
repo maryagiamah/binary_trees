fefe14eb3020960b64e25e5953916a3735500aa5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 05:18:47 +0100

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,28 @@
 #include "binary_trees.h"
 /**
+* binary_tree_sibling - finds the sibling of a node
+* @node: node to find sibling
+*
+* Return: sibling or NULL
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *p = NULL;
+
+	if (!node || !node->parent)
+		return (NULL);
+	p = node->parent;
+
+	if (p->left == p->right)
+		return (node);
+	if (p->left && p->left != node)
+		return (p->left);
+	if (p->right && p->right != node)
+		return (p->right);
+	return (NULL);
+}
+
+/**
 * binary_tree_uncle - finds the uncle of a node
 * @node: node to find uncle
 *
