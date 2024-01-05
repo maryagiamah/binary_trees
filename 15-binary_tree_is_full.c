84efb57d145c7b8278ac36e2be9a71f2eb387e46
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 12:19:42 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,13 +7,15 @@
 */
 size_t binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left_node, right_node;
+
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	/* Calculate the heights of left and right subtrees */
-	int left_node = binary_tree_is_full(tree->left);
-	int right_node = binary_tree_is_full(tree->right);
+	left_node = binary_tree_is_full(tree->left);
+	right_node = binary_tree_is_full(tree->right);
 
 	/* Calculate and return the balance factor */
 	return (left_node && right_node);
