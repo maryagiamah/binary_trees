1c18cef8bb52016e702aeb6fa8e3abcbdf906e9f
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 05:16:50 +0100

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,9 +7,7 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent
-|| !node->parent->parent)
+	if (!node || !node->parent)
 		return (NULL);
-	return (binary_tree_sibling(node->parent
-->parent));
+	return (binary_tree_sibling(node->parent));
 }
