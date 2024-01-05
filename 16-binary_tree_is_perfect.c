57e4d62336d9af794f262f92ea30c2c1c9afb8f5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-05 13:44:24 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,12 +36,12 @@ size_t binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t depth, size, max_size;
 
-	depth = binary_tree_depth(tree) + 1;
+	depth = binary_tree_depth(tree);
 	size = binary_tree_size(tree);
 
 	max_size = 1;
 
-	while (depth > 0)
+	while (depth > -1)
 	{
 		max_size *= 2;
 		depth--;
